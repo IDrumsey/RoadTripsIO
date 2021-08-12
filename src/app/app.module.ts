8679431eab5c884132ae90ba4d5d2f7f2283ea1e
IDrumsey <ccasper3@kent.edu>
2021-08-12 15:25:52 -0400

    Initial commit with layout template and website title added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
+// Native
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Third Party
+import { GoogleMapsModule } from '@angular/google-maps';
+
+// Local
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +15,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
