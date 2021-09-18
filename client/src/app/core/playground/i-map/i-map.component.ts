84c925435f7572c441d15fcf943c9608d826b68d
IDrumsey <ccasper3@kent.edu>
2021-09-17 22:28:17 -0400

    Finished most of the imap ui component which takes care of most the crud ops of markers in the map and persists click events up.

--- client/src/app/core/playground/i-map/i-map.component.ts
+++ client/src/app/core/playground/i-map/i-map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, Output, EventEmitter, Input } from '@angular/core';
 import { GoogleMap } from '@angular/google-maps';
 import { IMapService } from '../../services/maps/i-map.service';
 
@@ -16,11 +16,17 @@ export class IMapComponent implements OnInit, AfterViewInit {
       new google.maps.Marker({position: {lat: 42.548336429673455, lng: -72.46051052640635}}),
       new google.maps.Marker({position: {lat: 40.548336429673455, lng: -70.46051052640635}})
       )
+
+    this.selectedMarkers.forEach(selectedMarker => {
+      this.selectMarker(selectedMarker)
+    })
   }
 
   ngAfterViewInit(): void {
     if(this.mapComponent.googleMap){
       this.map = this.mapComponent.googleMap
+
+      this.map.setOptions(this.generateMapOptions())
     }
 
     this.fitAllMarkersInView()
@@ -28,8 +34,32 @@ export class IMapComponent implements OnInit, AfterViewInit {
 
   // --------------------------------- PROPERTIES ---------------------------------
   markers: google.maps.Marker[] = []
-  @ViewChild("map") mapComponent: GoogleMap
+  selectedMarkers: google.maps.Marker[] = []
+  @ViewChild("map") private mapComponent: GoogleMap
   map: google.maps.Map
+
+  // --------------------------------- STATE ---------------------------------
+  @Input() cursor: string
+  @Input() defaultMarkerIcon: string = "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+
+  // --------------------------------- EVENTS ---------------------------------
+  @Output() mapClick = new EventEmitter<google.maps.LatLng>()
+  @Output() markerClick = new EventEmitter<google.maps.Marker>()
+
+  // --------------------------------- EVENT HANDLERS ---------------------------------
+  
+  onMapClickEvent(mapEvent: google.maps.MapMouseEvent): void {
+    this.mapClick.emit(mapEvent.latLng)
+  }
+
+  onMarkerClick(markerClicked: google.maps.Marker): void {
+    // find the marker because markerClicked is a copy
+    let actualMarkerClicked = this.markers.find(marker => marker == markerClicked)
+    if(actualMarkerClicked){
+      this.markerClick.emit(markerClicked)
+      this.selectMarker(markerClicked)
+    }
+  }
 
   // --------------------------------- METHODS ---------------------------------
   /**
@@ -65,5 +95,48 @@ export class IMapComponent implements OnInit, AfterViewInit {
     let bounds = new google.maps.LatLngBounds({lat: westCoordinate.lat, lng: southCoordinate.lng}, {lat: eastCoordinate.lat, lng: northCoordinate.lng})
 
     return bounds
+  }
+
+  private generateMapOptions(): google.maps.MapOptions {
+    return {
+      draggableCursor: this.cursor
+    }
+  }
+
+  /**
+   * Zooms in on a marker
+   * @param marker The marker on the map to zoom in on
+   */
+  zoomInOnMarker(marker: google.maps.Marker): void {
+    let zoomLevel = 15
+
+    // https://stackoverflow.com/questions/5054515/zoom-in-to-marker-google-maps
+    let position = marker.getPosition()
+    if(position){
+        this.map.setZoom(zoomLevel)
+        this.map.panTo(position)
+    }
+  }
+
+  addMarker(markerToAdd: google.maps.Marker): void {
+    this.markers.push(markerToAdd)
+  }
+
+  removeMarker(markerToRemove: google.maps.Marker): void {
+    let indexToRemove = this.markers.findIndex(tempMarker => tempMarker == markerToRemove)
+    if(indexToRemove != -1) {
+      this.markers.splice(indexToRemove, 1)
+    }
+  }
+
+  selectMarker(markerToSelect: google.maps.Marker): void {
+    this.selectedMarkers.push(markerToSelect)
+    markerToSelect.setIcon("http://maps.google.com/mapfiles/ms/icons/blue-dot.png")
+  }
+
+  getMarkerOptions(marker: google.maps.Marker): google.maps.MarkerOptions {
+    return {
+      icon: marker.getIcon() ? marker.getIcon() as string : this.defaultMarkerIcon
+    }
   }
 }
